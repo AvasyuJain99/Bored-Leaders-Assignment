3a17eff942a6d31d08c50a59d79dd6cc33ea341d
AvasyuJain99 <109269204+AvasyuJain99@users.noreply.github.com>
2023-07-03 13:50:02 +0530

    Initial commit

---
+++ Assets/Scripts/Hurdle.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Hurdle")]
+public class Hurdle : ScriptableObject
+{
+    public string hurdleName;
+    public GameObject hurdlePrefab;
+    public int hurdleCount;
+ 
+}
